e216ad96de21e7f7cb0bdeed7c6d8ceff6984238
Ahmad Ozair Yousufi <110103215+AOYousufi@users.noreply.github.com>
2026-09-26 00:03:26 +0100

    Fix My Plants project link

--- src/pages/MyPlants.jsx
+++ src/pages/MyPlants.jsx
@@ -28,7 +28,7 @@ function MyPlants() {
 
         <div className="project-detail-links">
           <a
-            href="https://github.com/AOYousufi/my-plants"
+            href="https://github.com/AOYousufi/my-plants-FE"
             target="_blank"
             rel="noopener noreferrer"
             className="project-detail-btn"
